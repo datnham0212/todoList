5e375ad5b268d53ae3c2de84331b901a18d540d3
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-07-03 11:48:11 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 var taskCount = 0;
+
+let taskItems = [];
+
+
 function add_task() {
     var task = document.getElementById("task-input").value;
+    
+    // taskItems.push(task);
+    // localStorage.setItem("taskItems", JSON.stringify(taskItems));
+
     var ul = document.getElementById("todo-list");
 
     if(task !== ""){
         var li = document.createElement("li");
+        var removeButton = document.createElement("button");
+        removeButton.setAttribute("id", "remove-button");
+        removeButton.innerHTML = "Done?";
+        
+        removeButton.setAttribute("onclick", "remove_task()"); 
+        
+        removeButton.onclick = function() {
+            remove_task(li);
+        };
+
         li.append(document.createTextNode(task));
+        li.append(removeButton);
         ul.appendChild(li);
 
-        if(taskCount >= 20){
+        if(taskCount >= 9){
             taskCount = taskCount - 1;
             ul.removeChild(li);
         }
@@ -17,3 +36,11 @@ function add_task() {
         document.getElementById("task-count").innerHTML = taskCount;
     }
 }
+
+function remove_task(li) {
+    var ul = document.getElementById("todo-list"); 
+    ul.removeChild(li);
+}
+
+// console.log(localStorage.getItem("taskItems"));
+
